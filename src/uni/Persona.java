5c9a0cb7b04842703ceea451a991f88965a9dcd7
Roberto <roberto.brugnera@buonarroti.tn.it>
2021-12-11 11:11:28 +0100

    vibv

--- src/uni/Persona.java
+++ src/uni/Persona.java
@@ -1,4 +1,15 @@
 package uni;
 
 public class Persona {
+    String nome;
+    String cognome;
+    String codiceFiscale;
+    String dataDiNascita;
+
+    public Persona(String nome, String cognome, String codiceFiscale, String dataDiNascita) {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.codiceFiscale = codiceFiscale;
+        this.dataDiNascita = dataDiNascita;
+    }
 }
--- src/uni/Studente.java
+++ src/uni/Studente.java
@@ -1,6 +1,33 @@
 package uni;
 
+import sun.text.normalizer.NormalizerBase;
+
+import java.util.Objects;
+
 public class Studente  extends Persona{
     String matricola;
-    String tipo
+    String tipoLaurea;
+    int annoImmatricolazione;
+    int annoLaura;
+
+    public Studente() {
+    }
+
+    public Studente(String nome, String cognome, String codiceFiscale, String dataDiNascita, String matricola, String tipoLaurea, int annoImmatricolazione, int annoLaura) {
+        super(nome, cognome, codiceFiscale, dataDiNascita);
+        this.matricola = matricola;
+        this.tipoLaurea = tipoLaurea;
+        this.annoImmatricolazione = annoImmatricolazione;
+        this.annoLaura = annoLaura;
+    }
+
+    public String getMatricola() {
+        return matricola;
+    }
+
+    public void setMatricola(String matricola) {
+        this.matricola = matricola;
+    }
+
+
 }
